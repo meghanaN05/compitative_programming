e5e785a23aec44783181ce491f3fb40ca4a5af6b
meghanaN05 <meghananoochila@gmail.com>
2024-10-23 22:02:59 +0530

    Add files via upload

---
+++ A_Gregor_and_Cryptography.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long
+#define endl '\n'
+
+void solve() {
+    int p;
+    cin>>p;
+    cout<<2<<" "<<p-1<<" "<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int k;
+    cin >> k;
+
+    while (k--) {
+        solve();
+    }
+
+    return 0;
+
+}
